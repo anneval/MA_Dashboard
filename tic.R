ee2daf2639a5a48e041b10efdadd09f99854a4f2
anneval <72460239+anneval@users.noreply.github.com>
2022-02-11 16:45:10 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -1,3 +1,23 @@
 # TODO: Define steps using the tic DSL, see ?DSL
 # get_stage("<stage name>") %>%
 #   add_step(step_...(...))
+get_stage("install") %>%
+  add_step(step_install_cran("dataseries")) %>%
+  add_step(step_install_cran("dygraphs")) %>%
+  add_step(step_install_cran("flexdashboard")) %>%
+  add_step(step_install_cran("htmlwidgets")) %>%
+  add_step(step_install_cran("knitr")) %>%
+  add_step(step_install_cran("timetk")) %>%
+  add_step(step_install_cran("tsbox"))%>%
+  add_step(step_install_cran("readr"))%>%
+  add_step(step_install_cran("dplyr")) %>%
+  add_step(step_install_cran("ggplot2")) %>%
+  add_step(step_install_cran("tidyr")) %>%
+
+get_stage("before_deploy") %>%
+  add_step(step_setup_ssh()) %>%
+  add_step(step_setup_push_deploy())
+
+get_stage("deploy") %>%
+  add_code_step(rmarkdown::render("docs/index.Rmd")) %>%
+  add_step(step_do_push_deploy(commit_paths = "docs/index.html"))
