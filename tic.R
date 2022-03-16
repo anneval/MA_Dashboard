11ec0a07ca0c17fd08e640d19dfb2ce07b59b95d
anneval <72460239+anneval@users.noreply.github.com>
2022-03-16 10:31:30 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -20,4 +20,4 @@ get_stage("before_deploy") %>%
 
 get_stage("deploy") %>%
   add_code_step(rmarkdown::render("docs/index.Rmd")) %>%
-  add_step(step_do_push_deploy(commit_paths = "docs/index.html"))
+   add_step(step_do_push_deploy())
