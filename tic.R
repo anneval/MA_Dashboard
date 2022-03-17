12848f20e9c8b986400c5e4c393c3e5309557363
anneval <72460239+anneval@users.noreply.github.com>
2022-03-17 10:59:24 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -7,6 +7,7 @@ get_stage("install") %>%
   add_step(step_install_cran("timetk")) %>%
   add_step(step_install_cran("tsbox")) %>%
   add_step(step_install_cran("readxl")) %>%
+  add_step(step_install_cran("stargazer")) %>%
   add_step(step_install_cran("ggplot2"))
 
 get_stage("before_deploy") %>%
